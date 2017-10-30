1c53a7b0d03b565dfb22b2a6e75d00745b6f5203
scheirer-cassie <scheirer-cassie@users.noreply.github.com>
2017-10-30 23:06:22 +0000

    20171030T230614Z

--- mario.c
+++ mario.c
@@ -1,29 +1,38 @@
 #include <stdio.h>
 #include <cs50.h>
-#include <math.h> // call all modules
+#include <math.h>
 
-int main(void) // start c program
+int main(void)
 {
-    int height = -2; // set initial height to value that's less than the amount of rows that work
-    while (height <= -1 || height >= 24) // repeat this until height is greater than -1 or less than 24
+    // set initial height to value that's less than the amount of rows that work
+    int height = -2;
+    // repeat this until height is greater than -1 or less than 24
+    while (height <= -1 || height >= 24)
     {
-        height = get_int("Height:\n"); // get height from user if height <= 1 or >= 23
+        // get height from user if height <= 1 or >= 23
+        height = get_int("Height:\n");
     }
 
-    for (int row = height; row > 0; row--) // set number of rows to height; while number of rows is greater than 0, do this,
-                                           // and subract 1 from rows every time
+    // set number of rows to height; while number of rows is greater than 0, do this,
+    // and subract 1 from rows every time
+    for (int row = height; row > 0; row--)
     {
-        for (int spaces = 0; spaces < row - 1; spaces++) // set number of spaces to 0; while number of spaces is less than
-                                                         // number of rows - 1, do this, and add 1 to spaces every time
+        // set number of spaces to 0; while number of spaces is less than
+        // number of rows - 1, do this, and add 1 to spaces every time
+        for (int spaces = 0; spaces < row - 1; spaces++)
         {
-            printf(" ");    // print one space
+            // print one space
+            printf(" ");
         }
-        for (int hashes = 0; hashes < (height + 2) - row; hashes++) // set number of hashes to 0; while number of hashes is
-                                                                    // less than height plus 2 - number of rows, do this, and
-                                                                    // add 1 to hashes every time
+        // set number of hashes to 0; while number of hashes is
+        // less than height plus 2 - number of rows, do this, and
+        // add 1 to hashes every time
+        for (int hashes = 0; hashes < (height + 2) - row; hashes++)
         {
-            printf("#"); // print one hashtag
+            // print one hashtag
+            printf("#");
         }
-        printf("\n"); // print one line
+        // print one line
+        printf("\n");
     }
 }
